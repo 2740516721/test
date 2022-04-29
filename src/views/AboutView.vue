83d71bd119dc490850bfe07d37edd4a6de9cbd46
lxchost_pro <lxchost_glb@qq.com>
2022-04-29 23:59:54 +0800

    更改了关于页内容

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <header>
+      <div class="header">
+        <div class="title"><h1>水果商城</h1></div>
+        <div class="search">
+          <input type="text" placeholder="请输入您想搜索的商品">
+          <button>搜索</button>
+        </div>
+      </div>
+    </header>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+ header{
+   width: 100%;
+   height: 100px;
+   background-color: cornsilk;
+ }
+ .header{
+   width: 1126px;
+   height: 100px;
+   margin: 0 auto;
+   display: flex;
+   
+ }
+</style>
